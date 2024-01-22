1c95cf10ae5d9ff3a96bb033740c2102833dad29
dextrodeath <johnofknoch@gmail.com>
2024-01-22 02:41:40 -0500

    added new .glb & .usdz files

--- src/app/components/Model.js
+++ src/app/components/Model.js
@@ -5,8 +5,8 @@ const Model = () => {
     <div id="card">
       <model-viewer
         id="model-viewer"
-        src="/art.glb"
-        ios-src="/art.usdz"
+        src="/frame_art.glb"
+        ios-src="/frame_art.usdz"
         poster="/art.jpg"
         alt="A 3D model of an astronaut"
         shadow-intensity="1"
