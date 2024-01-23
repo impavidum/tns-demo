7dce7f9b3d072eb1814d63cf2b76b5c38ea662e1
a <a@as-MBP-2.lan>
2024-01-23 01:12:03 -0600

    Change model src

--- src/app/components/Model.js
+++ src/app/components/Model.js
@@ -5,8 +5,8 @@ const Model = () => {
     <div id="card">
       <model-viewer
         id="model-viewer"
-        src="/frame_art.glb"
-        ios-src="/frame_art.usdz"
+        src="/art.glb"
+        ios-src="/art.usdz"
         poster="/art.jpg"
         alt="A 3D model of an astronaut"
         shadow-intensity="1"
