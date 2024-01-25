90bc47206a7a991d5b98b6c1d6dbbac69ca9fde5
a <a@as-MBP-2.lan>
2024-01-25 16:28:52 -0600

    add only painting back

--- src/app/components/Model.js
+++ src/app/components/Model.js
@@ -5,6 +5,18 @@ const Model = () => {
     <div id="card">
       <model-viewer
         id="model-viewer"
+        src="/art.glb"
+        ios-src="/art.usdz"
+        poster="/art.jpg"
+        alt="A 3D model of an astronaut"
+        shadow-intensity="0"
+        camera-controls
+        auto-rotate
+        ar
+        ar-placement="wall"
+      ></model-viewer>
+      <model-viewer
+        id="model-viewer"
         src="/owl4.glb"
         ios-src="/owl.usdz"
         poster="/owl.jpg"
