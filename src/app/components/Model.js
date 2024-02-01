5daaa5ed6ccc83f543a4a03a51ab6a481a40eb6c
a <a@as-MBP-2.lan>
2024-02-01 13:26:58 -0600

    refactor for use with tns

--- src/app/components/Model.js
+++ src/app/components/Model.js
@@ -2,14 +2,11 @@
 
 const Model = () => {
   
-  return (
-    <div>
-
-      {/* Chalk Painting */}
-      <model-viewer
-        src="/art.glb"
-        ios-src="/art.usdz"
-        poster="/art.jpg"
+  return <model-viewer
+        src="/tns.glb"
+        ios-src="/tns.usdz"
+        // poster="/art.jpg"
+        orientation="180deg 0deg 0deg"
         alt="A 3D model of an astronaut"
         shadow-intensity="0"
         camera-controls
@@ -17,35 +14,12 @@ const Model = () => {
         ar-placement="wall"
         touch-action="pan-y"
         disable-pan
-        skybox-image="/art.jpg"
-        exposure="4"
-        min-camera-orbit="-55deg 45deg auto"
-        max-camera-orbit="55deg 120deg auto"
+        // skybox-image="/tns-gradient.png"
+        exposure="1"
+        min-camera-orbit="auto 90deg auto"
+        max-camera-orbit="auto 90deg auto"
         tone-mapping="commerce"
       ></model-viewer>
-  
-      {/* Owl Painting */}
-      <model-viewer
-        src="/Owl.glb"
-        ios-src="/OwlFramed.usdz"
-        orientation="90deg 0deg 90deg"
-        poster="/Owl.jpg"
-        alt="A 3D model of an astronaut"
-        shadow-intensity="0"
-        camera-controls
-        ar
-        ar-placement="wall"
-        disable-pan
-        skybox-image="/Owl.jpg"
-        exposure="4"
-        touch-action="pan-y"
-        min-camera-orbit="-55deg 45deg auto"
-        max-camera-orbit="55deg 120deg auto"
-        tone-mapping="commerce"
-      ></model-viewer>
-    </div>
-   
-  );
 };
 
 export default Model;
--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,22 @@ import Model from "./components/Model";
 export default function Home() {
   return (
     <main>
-      <div class="heading">
-      <h1>Art in AR</h1>
-      <p>View Art in your space by tapping the AR icon in the bottom right of the element on mobile.</p>
+
+      {/* Map creator controller  */}
+      <div className="controller">
+          <img src="/tns-logo-dark.svg" alt="" />
       </div>
+
+      {/* Map 3d model */}
       <Model />
+
+      {/* mode-view cdn  */}
       <script
         type="module"
         src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.0.1/model-viewer.min.js"
         async
       ></script>
+
     </main>
   );
 }
