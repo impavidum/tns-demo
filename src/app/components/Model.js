029c0b0bde16abc1d642a4d58f03e0ab0463984f
dextrodeath <johnofknoch@gmail.com>
2024-02-01 21:52:18 -0600

    .glb updated

--- src/app/components/Model.js
+++ src/app/components/Model.js
@@ -3,7 +3,7 @@
 const Model = () => {
   
   return <model-viewer
-        src="/tns.glb"
+        src="/tns-1.glb"
         ios-src="/tns.usdz"
         // poster="/art.jpg"
         orientation="180deg 0deg 0deg"
